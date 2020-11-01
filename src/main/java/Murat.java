bbc7201e47ad9c0d27282cd81ac6767026374a69
Murat <degirmenci_murat@yahoo.com>
2020-11-01 11:05:32 -0500

    Murat file updated

--- src/main/java/Murat.java
+++ src/main/java/Murat.java
@@ -1,1 +1,16 @@
-// Tried again
+public class Murat {
+
+    public static void main(String[] args) {
+
+        System.out.println("Superman was here");
+
+        System.out.println("Batman left");
+
+
+        // Are we enjoying
+
+
+    }
+
+}
+
