36bc004f7e77d3fae244d16ecc3b67d77f8bb402
Murat <degirmenci_murat@yahoo.com>
2020-11-01 10:28:02 -0500

    Batman Class

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,2 +1,13 @@
 public class Batman {
+
+    public static void main(String[] args) {
+
+        System.out.println("Superman was here");
+
+        System.out.println("Batman left");
+
+
+    }
+
+
 }
